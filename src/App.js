c3cffde90529820599926c7ac6be26b40e873a6e
Marcelo Menezes <contato@marcelomenezes.nom.br>
2024-03-24 21:57:46 +0100

    favicon and header changed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-import SearchPage from './components/SearchPage';
-import MovieDetails from './components/MovieDetails';
-import { Routes, Route } from 'react-router-dom';
-
+import Header from './components/Header';
+import MovieApp from './components/MovieApp';
+import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<SearchPage />} />
-      <Route path="/movie" element={<SearchPage />} />      
-      <Route path="/movie/:imdbId" element={<MovieDetails />} />
-    </Routes>
-
+    <div className="App">
+      <Header />
+      <MovieApp />
+      <Footer />
+    </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Image from 'react-bootstrap/Image';
 
 function Header() {
   return (
     <Navbar expand="lg" bg="dark" variant="dark" sticky="top">
       <Container>
-        <Navbar.Brand href="/">Blockbuster Search</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <Image src="/popcorn.png" width="50px" height="50px"  alt="Blockbuster Search" />Blockbuster Search</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/movie">Search</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
